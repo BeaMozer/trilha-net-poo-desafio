f4a8d0fb51a7a15dcdafb2c4a54505ca11874c79
BeaMozer <beatriz.mozer@hotmail.com>
2024-01-09 17:45:43 -0300

    Desafio POO conluido!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Implementado!
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "99797-0001", modelo: "C01 Plus", imei: "000005241000365", memoria: 32 );
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "99747-0002", modelo: "5c", imei: "111111111152365", memoria: 8 );
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
